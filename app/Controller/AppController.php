db1b344359ebd70cd086b5bb779f96c71d69839f
Jesse Cotton <jcotton@bitlancer.com>
2013-04-19 18:24:17 +0000

    Remove table testing code

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -40,29 +40,4 @@ class AppController extends Controller {
     	$this->Cookie->key = '>qfAHms1U1c{}0wC6SWZjur#!31TaB58LJQqathasGb$@11~_+!@#HKis~#^';
     	$this->Cookie->httpOnly = true;
 	}
-
-	public function parseDatatablesRequest($model,$request,$columns){
-		
-		$findParameters = array();
-		
-		if(isset($request['iDisplayStart']) && $request['iDisplayLength'] != '-1'){
-			$findParameters['offset'] = $request['iDisplayStart'];
-			$findParameters['limit'] = $request['iDisplayLength'];
-		}
-		
-		$findParameters['order'] = array();
-		if(isset($request['iSortCol_0']))
-		{
-			for ($i=0;$i<intval($request['iSortingCols']);$i++)
-			{
-				if($request['bSortable_'.intval($request['iSortCol_'.$i])] == "true")
-				{
-					$findParameters['order'][] = $model.".".$columns[intval($request['iSortCol_'.$i])] . " " . 
-						$request['sSortDir_'.$i];
-				}
-			}
-		}
-
-		return $findParameters;
-	}
 }
